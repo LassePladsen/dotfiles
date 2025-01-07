4c0bd99c3859d30f1bf5262f64ba33b460fc6387
Lasse Pladsen <lasse.pladsen@hotmail.com>
2025-01-07 18:49:22 +0100

    Fully working script for dotfiles

--- do_job_on_remotes.py
+++ do_job_on_remotes.py
@@ -29,6 +29,9 @@ DEFAULT_REMOTES = [
 
 
 class Job:
+    INFO_VERBOSITY = 0
+    STDOUT_VERBOSITY = 1
+
     def __init__(self, argv: Iterable = []):
         self.argv = argv
         self.args = None
@@ -70,54 +73,79 @@ class Job:
         # parse arguments and call job function
         self.args = parser.parse_args(self.argv)
 
-        # Parse comma-separated remotes list
-        self.args.remotes = self.args.remotes
+        # Parse comma-separated remotes list. Supports a single string
+        if isinstance(self.args.remotes, str):
+            self.args.remotes = self.args.remotes.split(",")
+
+        # Strip empty remote strings in list
+        while not self.args.remotes[-1]:
+            self.args.remotes = self.args.remotes[:-1]
 
         match self.args.job:
             case "dotfiles":
-                result = self.__scp_dotfiles()
+                self.__scp_dotfiles()
             case "command":
-                result = self.__ssh_command()
+                self.__ssh_command()
             case _:
                 raise ValueError(
                     f"Unknown job {self.args.job}. The choices are {JOBS}")
 
-        # I think its better to raise error if job failed inside the job function, so this else could be removed, instead catching the error.
-        self.__verbose_print(
-            "\nJobs done.\n", 0, color=Fore.GREEN) if result else print("Job failed.")
+        if self.failed_tasks:
+            self.__verbose_print(
+                f"\n{self.failed_tasks} tasks failed for job {self.args.job}.", 0, color=Fore.RED)
+        else:
+            self.__verbose_print("\nJobs done.\n", 0, color=Fore.GREEN)
 
     def __scp_dotfiles(self) -> bool:
         """Scp dotfiles and dependencies to each remote as the given user."""
-        remotes = [self.args.remotes,
-                   ] if self.args.remotes is str else self.args.remotes.split(",")
+        remotes = self.args.remotes
         user = self.args.user
 
+        self.failed_tasks = 0
         for remote in remotes:
-            self.__verbose_print(f"\nStarting on remote {
-                                 remote}", color=Fore.YELLOW)
+            self.__verbose_print(
+                f"\nStarting on remote {remote}", color=Fore.YELLOW)
 
+            # DOTFILES
             self.__verbose_print(f"Sending .dotfiles to remote {remote}...")
             if not self.__do_scp(f"{os.path.expanduser('~' + user)}/.bashrc {os.path.expanduser('~' + user)}/.tmux.conf {os.path.expanduser('~' + user)}/.vimrc {user}@{remote}:~{user}/"):
+                self.failed_tasks += 1
                 self.__verbose_print("Could not send dotfiles", color=Fore.RED)
+            # END DOTFILES
 
+            # TPM
             self.__verbose_print("Checking if tpm is installed...")
             if not self.__do_ssh_command(user, remote, f"test -d ~{user}/.tmux/plugins/tpm"):
                 self.__verbose_print(
                     "Tpm not installed - git cloning it...")
 
-                if not self.__do_ssh_command(user, remote, f"git clone https://github.com/tmux-plugins/tpm ~{user}.tmux/plugins/tpm"):
+
+                if not self.__do_ssh_command(user, remote, f"git clone https://github.com/tmux-plugins/tpm ~{user}/.tmux/plugins/tpm"):
+                    self.failed_tasks += 1
                     self.__verbose_print(
                         "Could not install tpm", color=Fore.RED)
+            # END TPM
+
+            # FZF
+            self.__verbose_print("Checking if fzf is installed...")
+            if not self.__do_ssh_command(user, remote, f"test -f ~{user}/.fzf/bin/fzf"):
+                self.__verbose_print("fzf not installed - git cloning it...")
+
+                if not self.__do_ssh_command(user, remote, f"git clone --depth 1 https://github.com/junegunn/fzf.git ~{user}/.fzf && ~{user}/.fzf/install", echo_to_ssh=True):
+                    self.failed_tasks += 1
+                    self.__verbose_print(
+                        "Could not install fzf", color=Fore.RED)
+            # END FZF
 
             self.__verbose_print(
                 f"Remote {remote} done!", color=Fore.BLUE)
-
-            # TODO: git clone fzf
         return True
 
-    def __verbose_print(self, msg: str, verbosity: int = 1, color: Fore = "", end: str = "\n") -> bool:
-        """Print message if job ran with verbosity equal to or greater parameter verbosity"""
-        if self.args.verbose >= verbosity:
+    def __verbose_print(self, msg: str, verbosity_level: int | None = None, color: Fore = "", end: str = "\n") -> bool:
+        """Print message if job verbosity is greater than or equal to parameter verbosity"""
+        verbosity_level = self.INFO_VERBOSITY if verbosity_level is None else verbosity_level
+
+        if self.args.verbose >= verbosity_level:
             print(color + msg + Style.RESET_ALL, end=end)
             return True
         return False
@@ -125,12 +153,19 @@ class Job:
     def __ssh_command(self) -> bool:
         raise NotImplementedError()
 
-    def __do_ssh_command(self, user: str, remote: str, cmd: str) -> bool:
-        return subprocess.run(
-            ["ssh", f"{user}@{remote}", cmd]).returncode == 0
+    def __do_ssh_command(self, user: str, remote: str, cmd: str, echo_to_ssh: bool = False) -> bool:
+        """Do a single ssh command on the remote"""
+        if echo_to_ssh:
+            # pipe echo command to ssh
+            ps = subprocess.Popen(["echo", cmd], stdout=subprocess.PIPE)
+            return subprocess.run(
+                ["ssh", f"{user}@{remote}"], capture_output=self.args.verbose < self.STDOUT_VERBOSITY, stdin=ps.stdout).returncode == 0
+        else:
+            return subprocess.run(
+                ["ssh", f"{user}@{remote}", cmd], capture_output=self.args.verbose < self.STDOUT_VERBOSITY).returncode == 0
 
     def __do_scp(self, args: str) -> bool:
-        return subprocess.run(["scp"] + args.split()).returncode == 0
+        return subprocess.run(["scp"] + args.split(), capture_output=self.args.verbose < self.STDOUT_VERBOSITY).returncode == 0
 
 
 if __name__ == "__main__":
