d58dce08ac77bdf05f62da0e84f7b6182ada961f
Lasse Pladsen <lasse.pladsen@hotmail.com>
2025-06-06 10:26:36 +0200

    hardcode discord + note setup, and mail fullscreen

--- .config/devilspie2/devilspie2.lua
+++ .config/devilspie2/devilspie2.lua
@@ -3,10 +3,27 @@ debug_print("Application: " .. get_application_name())
 debug_print("Window: " .. get_window_name());
 
 local window_name = get_window_name()
-if (string.match(window_name, "Obsidian") 
-    or string.match(window_name, "Discord") 
-    or string.match(window_name, "Spotify")) then
+if (string.match(window_name, "Spotify")) then
     pin_window() -- show on all workspaces
 end
+
+if (string.match(window_name, "Discord")) then 
+    set_viewport(1) -- left monitor
+    pin_window()
+    set_window_geometry(0,0,1280,1440) -- pin to left side of monitor
+end
+
+if (string.match(window_name, "Obsidian")) then
+    set_viewport(1) -- left monitor
+    pin_window()
+    set_window_geometry(1280,0,1280,720) -- pin to right side of monitor
+end
+
+if (window_name == "Mozilla Thunderbird") then
+    set_viewport(2) -- move to right monitor
+    maximize()
+    pin_window() -- show on all workspaces
+end
+
 
 
